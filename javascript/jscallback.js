85b8aee32599680bee291d323cdcf7d05636b3bf
woojin.kim <zheld7708@gmail.com>
2024-11-11 18:28:05 +0900

    선생님의 도움으로 태그 목록과 카테고리 복구

--- javascript/jscallback.js
+++ javascript/jscallback.js
@@ -1,25 +1,57 @@
-console.log(1);
-setTimeout(delayPrint, 1000);
-console.log(3);
+// console.log(1);
+// setTimeout(delayPrint, 1000);
+// console.log(3);
 
-function delayPrint(){
-    console.log(2)
-}
+// function delayPrint(){
+//     console.log(2)
+// }
 
 
 
-const myPromise =new Promise((resolvem, reject)=>{
-    const condition = true;
-    if(condition){
-        resolvem("성공");
-    }else{
-        reject("실패");
-    }
-});
+// const myPromise =new Promise((resolvem, reject)=>{
+//     const condition = true;
+//     if(condition){
+//         resolvem("성공");
+//     }else{
+//         reject("실패");
+//     }
+// });
 
-myPromise.then(res=>{
-    console.log(res);
-}).catch(err =>{
-    console.log(err);
-})
+// myPromise.then(res=>{
+//     console.log(res);
+// }).catch(err =>{
+//     console.log(err);
+// })
 
+
+// 음식 주문
+// 음식 주문 -> 음식을 요리 -> 테이블로 서빙
+
+//  비동기 작업에 대한 함수를 예시
+function oderFood(food) {
+    return new Promise((resolve) =>{
+      console.log(`주문을 받았습니다.(${food})`)
+      setTimeout(() =>{
+        resolve(`${food} 준비 완료`)
+      }, 2000) // 2초후에 주문 완료
+    })
+  }
+  
+  function serveFood(food) {
+    return new Promise((resolve) =>{
+      console.log(`${food} 서빙중...`)
+      setTimeout(()=>{
+        resolve(`${food} 서빙 완료`)
+      }, 1000)
+    })
+  }
+  
+  // async 와 await 사용
+  async function placeOder() {
+    const oderedFood = await oderFood('피자');
+    console.log(oderedFood)
+    const servedFood = await serveFood("피자");
+    console.log(servedFood);
+  }
+  
+  placeOder()
---
+++ node/project01/src/views/TestView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <h1>진짜 살기싫다</h1>
+  </div>
+</template>
